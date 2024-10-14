20c9b92688132b3538eef5dd33f15591d795cd81
baebaej <korea024504@gmail.com>
2024-10-14 18:18:56 +0900

    Feat: 0주차 과제

---
+++ hyunjun/week0/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import KakaoMap from "./KakaoMap";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Kakao 지도 예제</h1>
+      <KakaoMap />
+    </div>
+  );
+};
+
+export default App;
