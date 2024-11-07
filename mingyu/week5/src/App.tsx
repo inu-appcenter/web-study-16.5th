ccf60230a3aca3e3294a9a4228b1c0a7fdb48bde
2mingyu <tnt01010@naver.com>
2024-11-08 04:39:21 +0900

    Feat: 5주차 과제 코드 #15

--- mingyu/week5/src/App.tsx
+++ mingyu/week5/src/App.tsx
@@ -1,25 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootPage from "pages/RootPage";
+import LoginPage from "pages/LoginPage";
+import HomePage from "pages/HomePage";
+import PostPage from "pages/PostPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootPage />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/post/:postId" element={<PostPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
